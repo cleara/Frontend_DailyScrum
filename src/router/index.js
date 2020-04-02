0a2dbd9f153f3265c47faba4618f99035b9da9b4
cleara <clearasia_prastika_27rpl@student.smktelkom-mlg.sch.id>
2020-04-02 21:43:16 +0700

    alhamdulillah

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,51 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
+import Register from '../views/Register.vue'
+import DailyScrum from '../views/DailyScrum.vue'
+import Navbar from '../views/layouts/Navbar.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '/login',
+    name: 'login',
+    components: {default: Login}
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/register',
+    name: 'register',
+    components: {default: Register}
+  },
+  {
+    path: '/dailyscrum',
+    name: 'dailyscrum',
+    components:  {default:DailyScrum ,header: Navbar},
+    meta: { 
+      requiresAuth: true
   }
+}
+ 
 ]
 
+
 const router = new VueRouter({
-  routes
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: routes
+})
+
+router.beforeEach((to, from, next) => {
+  if(to.matched.some(record => record.meta.requiresAuth)) {
+    if (store.getters.isLoggedIn) {
+      next()
+      return
+    }
+    next('/login') 
+  } else {
+    next() 
+  }
 })
 
 export default router
